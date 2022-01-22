feb4042355399739a05c5355f1b241cb641e8d9d
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2022-01-22 18:36:54 +0800

    add Dapper + Repositiory.cs

---
+++ Models/Repository/FoodRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Tediu.FoodOrder.website.Models.EFModel;
+
+namespace Tediu.FoodOrder.website.Models.Repository
+{
+    interface IFoodRepository
+    {
+        void Create(FoodTableModel Model);
+        void Update(int id,FoodTableModel model);
+
+        List<FoodTableModel> getall();
+
+        List<FoodTableModel> getid();
+
+        void Delete(int id);
+    }
+    public class FoodRepository:IFoodRepository
+    {
+        private string datasouce =@"Data Source=TEDLIUCOMPUTER\SQLEXPRESS;Initial Catalog=DemoProject;Integrated Security=True";
+
+        public void Create(FoodTableModel Model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<FoodTableModel> getall()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<FoodTableModel> getid()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, FoodTableModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
