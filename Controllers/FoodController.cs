bd639a6bb5128e5b86de54371595c0ef5965b4e4
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2022-01-22 18:53:13 +0800

    Controller add  Food service  & and use Food.getall

---
+++ Controllers/FoodController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Tediu.FoodOrder.website.Models.Service;
+
+namespace Tediu.FoodOrder.website.Controllers
+{
+    public class FoodController : Controller
+    {
+        FoodService service = new FoodService();
+        // GET: FoodController
+        public ActionResult Index()
+        {
+            var result = service.getall();
+            return View(result);
+        }
+
+        // GET: FoodController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FoodController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FoodController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: FoodController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FoodController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: FoodController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Models/Service/FoodService.cs
+++ Models/Service/FoodService.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Tediu.FoodOrder.website.Models.EFModel;
 using Tediu.FoodOrder.website.Models.Repository;
 
 namespace Tediu.FoodOrder.website.Models.Service
 {
     public class FoodService
     {
-        FoodRepository repo = new FoodRepository();
+        private FoodRepository repo = new FoodRepository();
+
+        public List<FoodTableModel> getall()
+        {
+            return repo.getall();
+        }
+        
+
     }
 }
