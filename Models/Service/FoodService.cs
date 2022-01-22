3566fa7aef868b71208133b53ae14c08d39cb2e9
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2022-01-22 18:41:50 +0800

    add FoodService

---
+++ Models/Service/FoodService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Tediu.FoodOrder.website.Models.Repository;
+
+namespace Tediu.FoodOrder.website.Models.Service
+{
+    public class FoodService
+    {
+        FoodRepository repo = new FoodRepository();
+    }
+}
